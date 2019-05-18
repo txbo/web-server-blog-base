76795f7e727077b9d426c11b202320898a0b0674
tianxiaobo <tianxiaobo@meituan.com>
2019-05-18 12:51:15 +0800

    add log

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const querystring = require('querystring')
 const { get, set } = require('./src/db/redis')
+const { access } = require('./src/utils/log')
 const handleBlogRouter = require('./src/router/blog')
 const handleUserRouter = require('./src/router/user')
 
@@ -40,6 +41,8 @@ const getCookieExpires = () => {
 }
 
 const serverHandle = (req, res) => {
+  // 记录 accesslog
+  access(`${req.method} -- ${req.url} -- ${req.headers['user-agent']} -- ${Date.now()}`)
   // 设置返回格式
   res.setHeader('Content-type', 'application/json')
   // 获取 path
