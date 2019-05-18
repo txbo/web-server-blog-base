a6600067913d8fc2a9d60cb2687527f6e124e8c8
tianxiaobo <tianxiaobo@meituan.com>
2019-05-18 15:15:27 +0800

    secret md5

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,8 +1,9 @@
 const { exec, escape } = require('../db/mysql')
+const { genPassWord } = require('../utils/cryp')
 
 const login = (username, password) => {
   const escapeUserName = escape(username)
-  const escapePassWord = escape(password)
+  const escapePassWord = escape(genPassWord(password))
   const sql = `
     select username, realname from users
     where username=${escapeUserName} and password=${escapePassWord}
